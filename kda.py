503e090492d059d25a86b7ed0683dcd5f832b54a
kawã santos <155128233+Kawasantos@users.noreply.github.com>
2024-02-27 18:50:01 -0300

    Update KDA DE JOGO.py

--- kda.py
+++ kda.py
@@ -1,18 +1,14 @@
-kill = float(input('digite quantas kill voce fez:'))
+kill = float(input('Digite quantas kills você fez: '))
+death = float(input('Digite quantas vezes morreu: '))
+help = float(input('Digite quantas vezes ajudou os aliados: '))
 
-death = float(input("digite quantas vezes morreu:"))
+performance = (kill + death + help) / 3
 
-help = float(input("digite quantas vezes ajudou os aliados:"))
-
-performance =  (kill + death + help) /3
-
-if  performance <=6:
-    print(f'sua performace foi pessima foi {performance}, ajude mais a equipe nas proximas partidas')
-
-if performance >=7 or performance <=8:
-     print(f" voce foi muito bem, ajudou bastante em seu time,foi {performance} recebera prata em seu desempenho")
-
+if performance < 6:
+    print(f'Sua performance foi péssima: {performance}. Ajude mais a equipe nas próximas partidas.')
+elif 7 <= performance <= 8:
+    print(f'Você foi muito bem, ajudou bastante em seu time. Sua performance foi {performance}. Você receberá prata em seu desempenho.')
 else:
-     print(f'parabens, voce colaborou demias para seu time, recebera ouro em seu desempenho {performance}')
+    print(f'Parabéns! Você colaborou demais para seu time. Sua performance foi {performance}. Você receberá ouro em seu desempenho.')
 
 
